6d9547633afaca63e088935cfed7edc6e7cc7038
VR <vladrymarchuk@gmail.com>
2024-12-22 20:36:51 +0200

    feat: add home page view, create task, toggle status

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from core.views import (
+    home_page_view,
+    tags_page_view,
+    toggle_status,
+    CreateTaskView,
+)
+
+urlpatterns = [
+    path("", home_page_view, name="home"),
+
+    path("<int:task_id>/", toggle_status, name="toggle_status"),
+    path("create-task/", CreateTaskView.as_view(), name="create-task"),
+]
+app_name = "mini_to_do_list"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from core.models import Task
+
+
+def home_page_view(request: HttpRequest) -> HttpResponse:
+    tasks = Task.objects.prefetch_related("tags__tasks")
+    context = {"tasks": tasks}
+    return render(request, "home.html", context=context)
+
+
+def toggle_status(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect("core:home")
+
+
+class CreateTaskView(CreateView):
+    model = Task
+    fields = ["tags", "content", "deadline", "is_completed"]
+    template_name = "create_task_form.html"
+    success_url = reverse_lazy("core:home")
+
+
+
