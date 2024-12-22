61d4b1d65a10abc460c6ae95e5fe8ec4738c2da5
VR <vladrymarchuk@gmail.com>
2024-12-22 16:06:42 +0200

    feat: create new app 'core', correct do double quotes

--- mini_to_do_list/wsgi.py
+++ mini_to_do_list/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_to_do_list.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_to_do_list.settings")
 
 application = get_wsgi_application()
