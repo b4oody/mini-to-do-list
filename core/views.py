88fedd62dda9961390826ed6051a27a9d9f502e3
VR <vladrymarchuk@gmail.com>
2024-12-24 22:50:46 +0200

    update: change README.md, add requirements.txt

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
