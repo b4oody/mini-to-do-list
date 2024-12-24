583342baa3f45ee1aee9f3359cba21c0316b5ee3
VR <vladrymarchuk@gmail.com>
2024-12-24 20:21:41 +0200

    refactoring: convert all FBV to CBV

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from core.views import (
-    home_page_view,
-    tags_page_view,
-    toggle_status,
+    HomePageView,
+    TagsListView,
+    ToggleStatusView,
     CreateTaskView,
     UpdateTaskView,
     DeleteTaskView,
@@ -13,12 +13,13 @@ from core.views import (
 )
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
-    path("<int:task_id>/", toggle_status, name="toggle_status"),
+    path("", HomePageView.as_view(), name="home"),
+    # path("", home_page_view, name="home"),
+    path("<int:task_id>/", ToggleStatusView.as_view(), name="toggle_status"),
     path("create-task/", CreateTaskView.as_view(), name="create-task"),
     path("update-task/<int:pk>/", UpdateTaskView.as_view(), name="update-task"),
     path("delete/<int:pk>/", DeleteTaskView.as_view(), name="delete-task"),
-    path("tags/", tags_page_view, name="tags"),
+    path("tags/", TagsListView.as_view(), name="tags"),
     path("create-tag/", CreateTagView.as_view(), name="create-tag"),
     path("update-tag/<int:pk>/", UpdateTagView.as_view(), name="update-tag"),
     path("delete-tag/<int:pk>/", DeleteTagView.as_view(), name="delete-tag"),
--- core/views.py
+++ core/views.py
@@ -1,55 +1,52 @@
-from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect
+)
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views import View
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView
+)
 
 from core.form import StatusFilterForm
 from core.models import Task, Tag
 
 
-def pagination(request: HttpRequest, queryset, items_per_page=5):
-    """
-    Pagination function.
+class HomePageView(ListView):
+    model = Task
+    template_name = "home.html"
+    paginate_by = 3
 
-    Arguments:
-    request: HttpRequest - request object.
-    queryset: QuerySet or list - data to be divided into pages.
-    items_per_page: int - number of items per page (default 5).
+    def get_queryset(self):
+        tasks = Task.objects.prefetch_related("tags__tasks")
+        form = StatusFilterForm(self.request.GET)
+        if form.is_valid():
+            status = form.cleaned_data.get("status")
+            if status and status != "all":
+                if status == "completed":
+                    tasks = tasks.filter(is_completed=True)
+                elif status == "active":
+                    tasks = tasks.filter(is_completed=False)
+        return tasks
 
-    Returns:
-    page_obj: pagination object for the template.
-    """
-    paginator = Paginator(queryset, items_per_page)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    return page_obj
-
-
-def home_page_view(request: HttpRequest) -> HttpResponse:
-    tasks = Task.objects.prefetch_related("tags__tasks")
-    form = StatusFilterForm(request.GET)
-    if form.is_valid():
-        status = form.cleaned_data.get("status")
-        if status and status != "all":
-            if status == "completed":
-                tasks = tasks.filter(is_completed=True)
-            elif status == "active":
-                tasks = tasks.filter(is_completed=False)
-    page_obj = pagination(
-        request=request,
-        queryset=tasks,
-        items_per_page=5
-    )
-    context = {"page_obj": page_obj, "form": form}
-    return render(request, "home.html", context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = StatusFilterForm(self.request.GET)
+        return context
 
 
-def toggle_status(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    task.is_completed = not task.is_completed
-    task.save()
-    return redirect("core:home")
+class ToggleStatusView(View):
+    @staticmethod
+    def post(request: HttpRequest, task_id: id) -> HttpResponse:
+        task = get_object_or_404(Task, id=task_id)
+        task.is_completed = not task.is_completed
+        task.save()
+        return redirect("core:home")
 
 
 class CreateTaskView(CreateView):
@@ -71,18 +68,10 @@ class DeleteTaskView(DeleteView):
     success_url = reverse_lazy("core:home")
 
 
-def tags_page_view(request: HttpRequest) -> HttpResponse:
-    tags = Tag.objects.all()
-    page_obj = pagination(
-        request=request,
-        queryset=tags,
-        items_per_page=3
-    )
-    return render(
-        request,
-        "tags.html",
-        context={"page_obj": page_obj}
-    )
+class TagsListView(ListView):
+    model = Tag
+    template_name = "tags.html"
+    paginate_by = 3
 
 
 class CreateTagView(CreateView):
